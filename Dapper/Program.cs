4a5b618cfb865c492450f9d6c41167c2547c4c86
Harsh Sidhpuria <166816768+Hrsh-Sidhpuria@users.noreply.github.com>
2024-07-25 14:46:54 +0530

    Learning Dapper

---
+++ Dapper/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Dapper;
+using System.Data.SqlClient;
+
+string connectionString = "Server=DESKTOP-VGKVPEH\\SQLEXPRESS;Database=employees;Trusted_Connection=True;";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    Console.WriteLine("Database Connected Successfully...");
+}
+
